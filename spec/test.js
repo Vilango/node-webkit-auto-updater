4d113c287da1943037389ca6b75c9d3c96d66405
Gerwin Brunner <gerwin.brunner@gmail.com>
2014-08-25 19:16:44 +0200

    added event listener

--- spec/test.js
+++ spec/test.js
@@ -1,10 +1,14 @@
-var Updater = require("./lib/updater");
-var UpdaterError = require('./lib/updaterError');
+var Updater = require("./../lib/updater");
+var UpdaterError = require('./../lib/updaterError');
 var uuid = require('node-uuid');
 var path = require('path');
 
 var winston = require('winston');
 var logger = winston.loggers.get('updateLogger') || winston.loggers.add('updateLogger', {console: {label: "xxx"}, file: {name: "test.log"}});
+
+var ncp = require('ncp').ncp;
+ncp.limit = 16
+
 
 logger.info("logger initialized");
 
@@ -13,10 +17,13 @@ var u = new Updater();
 var tmpId = uuid.v4();
 
 
-var tmp_file = u.isFileUrl( "http://localhost:3006/TestApp/osx/TestApp.dmg" , function(err, data){
-  console.log("Return:",err,data);
-});
+// var tmp_file = u.isFileUrl( "http://localhost:3006/TestApp/osx/TestApp.dmg" , function(err, data){
+//   console.log("Return:",err,data);
+// });
 
+ncp("/Volumes/6148b320-ed75-4d39-ba7e-d55fe7f5eac0/TestApp.app", "/var/folders/fp/rt5y2x2x7qvghvsrf03jr6jh0000gn/T/xxxx2.TestApp.app", function(err){
+  console.log("copied files", err);
+});
 
 // var tmp_file = u.downloadAsTmpFile( "http://FAKEURL", tmpId , function(err, data){
 //   console.log("Return:",err,data);
--- spec/updater-spec.js
+++ spec/updater-spec.js
@@ -1,4 +1,7 @@
 require('fibrous/lib/jasmine_spec_helper');
+jasmine.DEFAULT_TIMEOUT_INTERVAL = (1000*10); // 10 Sekunden
+
+var fs = require('fs-extra');
 
 var winston = require('winston');
 winston.remove(winston.transports.Console);
@@ -6,10 +9,10 @@ winston.add(winston.transports.Console, { level: 'debug', colorize:true } );
 updateLogger = winston.loggers.add('updateLogger',     { console: {label: "updater", level: 'warn', colorize:true } } );
 
 var Updater = require("../lib/updater");
-var fs = require('fs-extra');
 var exec = require('child_process').exec;
 var uuid = require('node-uuid');
 var path = require('path');
+var uuid = require('node-uuid');
 
 winston.error("logger ready to run");
 updateLogger.error("logger ready to run");
@@ -27,88 +30,88 @@ huge_dmg_file_url = "http://www.greenheartgames.com/files_public/GameDevTycoonDE
 /////////////////////////////////////////
 
 
-// describe("updater.isFileUrl", function () {
-//   it("should correctly detect js file url", function () {
-//     var u = new Updater();
-//     expect(u.sync.isFileUrl(some_js_url)).toBe(false);
-//   });
-//   it("should correctly detect dmg file url", function () {
-//     var u = new Updater();
-//     expect(u.sync.isFileUrl(some_github_file_url)).toBe(true);
-//   });
-//   it("should correctly detect exe file url", function () {
-//     var u = new Updater();
-//     expect(u.sync.isFileUrl(some_win_file_url)).toBe(true);
-//   });
+describe("updater.isFileUrl", function () {
+  it("should correctly detect js file url", function () {
+    var u = new Updater();
+    expect(u.sync.isFileUrl(some_js_url)).toBe(false);
+  });
+  it("should correctly detect dmg file url", function () {
+    var u = new Updater();
+    expect(u.sync.isFileUrl(some_github_file_url)).toBe(true);
+  });
+  it("should correctly detect exe file url", function () {
+    var u = new Updater();
+    expect(u.sync.isFileUrl(some_win_file_url)).toBe(true);
+  });
 
-//   it("should correctly detect invalid url", function(done) {
-//     var u = new Updater();
-//     u.isFileUrl("http://FAKEURL", function(error, data){
-//       expect(error).not.toBe(null);
-//       expect(error.message).toBe("Can't read the file form url");
-//       done();
-//     });
+  it("should correctly detect invalid url", function(done) {
+    var u = new Updater();
+    u.isFileUrl("http://FAKEURL", function(error, data){
+      expect(error).not.toBe(null);
+      expect(error.message).toBe("Can't read the file form url");
+      done();
+    });
     
-//   });
-// }); 
+  });
+}); 
 
-// describe("updater.existsUrlForPlatform", function () {
-//   it("should have url for mac", function () {
-//     var u = new Updater({sources: {mac: "http:\\\\sample.com"}});
-//     expect(u.existsUrlForPlatform("mac")).toBe(true);
-//   });
-//   it("should not have url for mac", function () {
-//     var u = new Updater();
-//     expect(u.existsUrlForPlatform("mac")).toBe(false);
-//   });
-//   it("should fail with empty url", function () {
-//     var u = new Updater({sources: {mac: ""}});
-//     expect(u.existsUrlForPlatform("mac")).toBe(false);
-//   });
-//   it("should fail with bad url", function () {
-//     var u = new Updater({sources: {mac: "xf"}});
-//     expect(u.existsUrlForPlatform("mac")).toBe(false);
-//   });
-// }); 
+describe("updater.existsUrlForPlatform", function () {
+  it("should have url for mac", function () {
+    var u = new Updater({sources: {mac: "http:\\\\sample.com"}});
+    expect(u.existsUrlForPlatform("mac")).toBe(true);
+  });
+  it("should not have url for mac", function () {
+    var u = new Updater();
+    expect(u.existsUrlForPlatform("mac")).toBe(false);
+  });
+  it("should fail with empty url", function () {
+    var u = new Updater({sources: {mac: ""}});
+    expect(u.existsUrlForPlatform("mac")).toBe(false);
+  });
+  it("should fail with bad url", function () {
+    var u = new Updater({sources: {mac: "xf"}});
+    expect(u.existsUrlForPlatform("mac")).toBe(false);
+  });
+}); 
 
-// describe("updater.download", function () {
-//   it("should download a file", function () {
-//     var u = new Updater();
-//     var fileId = Math.floor((Math.random() * 10000) + 1)
-//     filename = "xxxx.download-"+fileId+".dmg";
+describe("updater.download", function () {
+  it("should download a file", function () {
+    var u = new Updater();
+    var fileId = uuid.v4()
+    filename = "xxxx.download-"+fileId+".dmg";
 
-//     u.sync.download( some_github_file_url, filename);
-//     expect(fs.sync.stat(filename).size).toBe(18317);
-//     fs.unlink(filename);
-//   });
-// });
+    u.sync.download( some_github_file_url, filename);
+    expect(fs.sync.stat(filename).size).toBe(18317);
+    fs.unlink(filename);
+  });
+});
 
-// describe("updater.downloadAsTmpFile", function () {
-//   it("should download a file", function () {
-//     var u = new Updater();
-//     var tmpId = uuid.v4();
-//     var tmp_file = u.sync.downloadAsTmpFile( some_github_file_url, tmpId );
-//     expect(fs.sync.stat(tmp_file).size).toBe(18317);
-//     fs.unlink(tmp_file);
-//   });
-// });
+describe("updater.downloadAsTmpFile", function () {
+  it("should download a file", function () {
+    var u = new Updater();
+    var tmpId = uuid.v4();
+    var tmp_file = u.sync.downloadAsTmpFile( some_github_file_url, tmpId );
+    expect(fs.sync.stat(tmp_file).size).toBe(18317);
+    fs.unlink(tmp_file);
+  });
+});
 
-// describe("updater.downloadAsTmpFile", function () {
-//   it("should download a file", function (done) {
-//     var u = new Updater();
-//     var tmpId = uuid.v4();
+describe("updater.downloadAsTmpFile", function () {
+  it("should download a file", function (done) {
+    var u = new Updater();
+    var tmpId = uuid.v4();
 
-//     u.downloadAsTmpFile( "http://FAKEURL", tmpId, function(error, data){
-//       expect(error.message).toBe("Can't download temp file from:http://FAKEURL");
-//       done();
-//     })
-//   });
-// });
+    u.downloadAsTmpFile( "http://FAKEURL", tmpId, function(error, data){
+      expect(error.message).toBe("Can't download temp file from:http://FAKEURL");
+      done();
+    })
+  });
+});
 
 describe("updater.findMountPoint", function () {
   it("should find mount point", function () {
     var testdmg = "Test.dmg";
-    var mountpointId = "xxxx.findMountPoint-"+Math.floor((Math.random() * 10000) + 1);
+    var mountpointId = "xxxx.findMountPoint-"+uuid.v4();
     var mountCopy = mountpointId+".dmg";
     exec.sync('cp spec/'+testdmg+' '+mountCopy);
 
@@ -131,126 +134,144 @@ describe("updater.findMountPoint", function () {
 });
 
 
-// describe("updater.mount", function () {
-//   it("should mount a file", function () {
-//     var testdmg = "Test.dmg";
-//     var mountCopy = "xxxx.mount-"+Math.floor((Math.random() * 10000) + 1)+".dmg";
-//     exec.sync('cp spec/'+testdmg+' '+mountCopy);
+describe("updater.mount", function () {
+  it("should mount a file", function () {
+    var testdmg = "Test.dmg";
+    var mountCopy = "xxxx.mount-"+uuid.v4()+".dmg";
+    exec.sync('cp spec/'+testdmg+' '+mountCopy);
 
-//     var mountpointId = "xxxx.mount-"+Math.floor((Math.random() * 10000) + 1);
-//     var u = new Updater();
-//     var mpoint = u.sync.mount( mountCopy, mountpointId );
-//     expect(mpoint).toBe("/Volumes/"+mountpointId);
-//     u.sync.unmount( mpoint );
-//     expect(fs.sync.stat(mountCopy).size).toBe(18317);
-//     fs.sync.unlink(mountCopy);
-//   });
-//   it("should fail to mount fake file", function(done) {
-//     var mountpointId = "xxxx.mount-"+Math.floor((Math.random() * 10000) + 1);
-//     var u = new Updater();
-//     u.mount( "FakeFile", mountpointId, function(error){
-//       expect(error).not.toBe(null);
-//       expect(error.message).toBe("Can't mount file: FakeFile at /Volumes/"+mountpointId);
-//       done();
-//     });
-//   });
-// });
+    var mountpointId = "xxxx.mount-"+uuid.v4();
+    var u = new Updater();
+    var mpoint = u.sync.mount( mountCopy, mountpointId );
+    expect(mpoint).toBe("/Volumes/"+mountpointId);
+    u.sync.unmount( mpoint );
+    expect(fs.sync.stat(mountCopy).size).toBe(18317);
+    fs.sync.unlink(mountCopy);
+  });
+  it("should fail to mount fake file", function(done) {
+    var mountpointId = "xxxx.mount-"+uuid.v4();
+    var u = new Updater();
+    u.mount( "FakeFile", mountpointId, function(error){
+      expect(error).not.toBe(null);
+      expect(error.message).toBe("Can't mount file: FakeFile at /Volumes/"+mountpointId);
+      done();
+    });
+  });
+  it("should mount the same file twice without error", function() {
+    var testdmg = "Test.dmg";
+    var mountCopy = "xxxx.mount-"+uuid.v4()+".dmg";
+    exec.sync('cp spec/'+testdmg+' '+mountCopy);
 
-// describe("updater.renameFile", function () {
-//   it("should mount a file", function () {
-//     var testdmg = "Test.dmg";
-//     var testCopy = "xxxx.renameFile-"+Math.floor((Math.random() * 10000) + 1)+".dmg";
-//     exec.sync('cp spec/'+testdmg+' '+testCopy);
+    var mountpointId = "xxxx.mount-"+uuid.v4();
+    var u = new Updater();
 
-//     var u = new Updater();
-//     var renFile = "xxxx."+testCopy;
-//     u.sync.renameFile( testCopy, renFile);
-     
-//     expect(fs.sync.stat(renFile).size).toBe(18317);
-//     fs.unlink(renFile);
-//   });
-// });
+    var mpoint = u.sync.mount( mountCopy, mountpointId );
+    expect(mpoint).toBe("/Volumes/"+mountpointId);
 
-// describe("updater.removeQuarantine", function () {
-//   it("should mount a file", function () {
-//     var quarantine = "com.apple.quarantine";
-
-//     var specApp= "unsecure.app";
-//     var copyApp = "xxxx.removeQuarantine-"+Math.floor((Math.random() * 10000) + 1)+".app";
-//     exec.sync('cp -R spec/'+specApp+' '+copyApp);
-
-//     var fileAttributes = exec.sync('xattr '+copyApp);
-//     expect(fileAttributes).toContain(quarantine);
-
-//     var u = new Updater();
-//     u.sync.removeQuarantine( copyApp );
+    var mpoint2 = u.sync.mount( mountCopy, "othermountpoint" );
+    expect(mpoint).toBe(mpoint2);
     
-//     var fileAttributes = exec.sync('xattr '+copyApp);
-//     expect(fileAttributes).toNotContain(quarantine);
+    u.sync.unmount( mpoint );
+    expect(fs.sync.stat(mountCopy).size).toBe(18317);
+    fs.sync.unlink(mountCopy);
+  });
+});
+
+describe("updater.renameFile", function () {
+  it("should mount a file", function () {
+    var testdmg = "Test.dmg";
+    var testCopy = "xxxx.renameFile-"+uuid.v4()+".dmg";
+    exec.sync('cp spec/'+testdmg+' '+testCopy);
+
+    var u = new Updater();
+    var renFile = "xxxx."+testCopy;
+    u.sync.renameFile( testCopy, renFile);
+     
+    expect(fs.sync.stat(renFile).size).toBe(18317);
+    fs.unlink(renFile);
+  });
+});
+
+describe("updater.removeQuarantine", function () {
+  it("should mount a file", function () {
+    var quarantine = "com.apple.quarantine";
+
+    var specApp= "unsecure.app";
+    var copyApp = "xxxx.removeQuarantine-"+uuid.v4()+".app";
+    exec.sync('cp -R spec/'+specApp+' '+copyApp);
+
+    var fileAttributes = exec.sync('xattr '+copyApp);
+    expect(fileAttributes).toContain(quarantine);
+
+    var u = new Updater();
+    u.sync.removeQuarantine( copyApp );
     
-//     expect(fs.sync.stat(copyApp).size).toBe(102);
-//     fs.remove(copyApp);
-//   });
-// });
+    var fileAttributes = exec.sync('xattr '+copyApp);
+    expect(fileAttributes).toNotContain(quarantine);
+    
+    expect(fs.sync.stat(copyApp).size).toBe(102);
+    fs.remove(copyApp);
+  });
+});
 
-// describe("updater.checkNewApp", function () {
-//   it("should check if there is an app in the dmg", function () {
-//     var testdmg = "Test.dmg";
-//     var dmgCopy = "xxxx.renameFile-"+Math.floor((Math.random() * 10000) + 1)+".dmg";
-//     exec.sync('cp spec/'+testdmg+' '+dmgCopy);
+describe("updater.checkExists", function () {
+  it("should check if there is an app in the dmg", function () {
+    var testdmg = "Test.dmg";
+    var dmgCopy = "xxxx.renameFile-"+uuid.v4()+".dmg";
+    exec.sync('cp spec/'+testdmg+' '+dmgCopy);
 
-//     var u = new Updater();
+    var u = new Updater();
 
-//     var mountpointId = "xxxx.mount-"+Math.floor((Math.random() * 10000) + 1);
-//     var mpoint = u.sync.mount( dmgCopy, mountpointId );
+    var mountpointId = "xxxx.mount-"+uuid.v4();
+    var mpoint = u.sync.mount( dmgCopy, mountpointId );
 
-//     var isAppBad = u.sync.checkNewApp( path.join(mpoint, "MissingFile.file") );
-//     expect(isAppBad).toBe(false);
+    var isAppBad = u.sync.checkExists( path.join(mpoint, "MissingFile.file") );
+    expect(isAppBad).toBe(false);
 
-//     var isAppOK = u.sync.checkNewApp( path.join(mpoint, "Readme.txt") );
-//     expect(isAppOK).toBe(true);
+    var isAppOK = u.sync.checkExists( path.join(mpoint, "Readme.txt") );
+    expect(isAppOK).toBe(true);
 
-//     u.sync.unmount( mpoint );
-//     fs.remove(dmgCopy);
-//   });
-// });
+    u.sync.unmount( mpoint );
+    fs.remove(dmgCopy);
+  });
+});
 
-// describe("updater.remove", function () {
-//   it("should remove file", function () {
-//     var testdmg = "Test.dmg";
-//     var testCopy = "xxxx.renameFile-"+Math.floor((Math.random() * 10000) + 1)+".dmg";
-//     exec.sync('cp spec/'+testdmg+' '+testCopy);
+describe("updater.remove", function () {
+  it("should remove file", function () {
+    var testdmg = "Test.dmg";
+    var testCopy = "xxxx.renameFile-"+uuid.v4()+".dmg";
+    exec.sync('cp spec/'+testdmg+' '+testCopy);
 
-//     expect( fs.existsSync(testCopy) ).toBe(true);
+    var u = new Updater();
+    expect( u.sync.checkExists(testCopy) ).toBe(true);
 
-//     var u = new Updater();
-//     u.sync.remove(testCopy);
+    u.sync.remove(testCopy);
      
-//     expect(fs.existsSync(testCopy)).toBe(false);
-//   });
-//   it("should fail removing none existing file", function () {
-//     var fakeFileName = "FakeFileName.xxx";
-//     expect( fs.existsSync(fakeFileName) ).toBe(false);
+    expect( u.sync.checkExists(testCopy)).toBe(false);
+  });
+  it("should fail removing none existing file", function () {
+    var fakeFileName = "FakeFileName.xxx";
+    var u = new Updater();
+    expect( u.sync.checkExists(fakeFileName) ).toBe(false);
 
-//     var u = new Updater();
-//     u.sync.remove(fakeFileName);
+    u.sync.remove(fakeFileName);
 
-//     expect( fs.existsSync(fakeFileName) ).toBe(false);
-//     // it does not fail if folder does not exist!!!!
-//   });
+    expect( u.sync.checkExists(fakeFileName) ).toBe(false);
+    // it does not fail if folder does not exist!!!!
+  });
 
-//   it("should remove an appdir", function () {
-//     var specApp= "unsecure.app";
-//     var copyApp = "xxxx.remove-"+Math.floor((Math.random() * 10000) + 1)+".app";
-//     exec.sync('cp -R spec/'+specApp+' '+copyApp);
-//     expect( fs.existsSync(copyApp) ).toBe(true);
+  it("should remove an appdir", function () {
+    var specApp= "unsecure.app";
+    var copyApp = "xxxx.remove-"+uuid.v4()+".app";
+    exec.sync('cp -R spec/'+specApp+' '+copyApp);
 
-//     var u = new Updater();
-//     u.sync.remove(copyApp);
-     
-//     expect(fs.existsSync(copyApp)).toBe(false);
-//   });
-// });
+    var u = new Updater();
+    expect(u.sync.checkExists(copyApp)).toBe(true);
+    
+    u.sync.remove(copyApp);
+    expect(u.sync.checkExists(copyApp)).toBe(false);
+  });
+});
 
 
 
